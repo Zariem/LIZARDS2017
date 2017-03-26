10ad482b504b2b6c33b965a7660f002376d15672
Virginia Ramp <vramp@student.ethz.ch>
2017-03-26 21:39:56 +0200

    tried feature extension

--- Task1.py
+++ Task1.py
@@ -1,5 +1,6 @@
 
 import numpy
+import math
 
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, make_scorer
@@ -31,6 +32,19 @@ def get_xsTrain(data):
 	bla = [line[1:] for line in data]
 	return bla
 
+def extendFeatures(data):
+    bla = []
+    for line in data:
+        new_line = []
+        for value in line:
+            new_line.append(value)
+            new_line.append(math.log(math.fabs(value)))
+            new_line.append(math.exp(value))
+            new_line.append(math.sqrt(math.fabs(value)))
+        new_line.append(math.fsum(new_line))
+        bla.append(new_line)
+    return bla
+
 def apply_lin_reg(xTrain, yTrain, xTest):
 	regressor = linear_model.LinearRegression()
 	regressor.fit(xTrain, yTrain)
@@ -52,22 +66,35 @@ def error(real_value, prediction):
 
 dataTrain = read_data()
 xsTrain = get_xsTrain(dataTrain)
+extendFeatures(xsTrain)
 ys = get_ys(dataTrain)
 dataTest = read_data(filepathTest)
+
+xsTrainExtended = extendFeatures(xsTrain)
+dataTestExtended = extendFeatures(dataTest)
 
 our_scorer = make_scorer(error, greater_is_better = False)
 predictors = []
 
-'''for x in range(1,10):
+for x in range(2,7):
     print(x)
     #predictor = svm.SVR(kernel = 'poly', degree=x)
+    #predictor = linear_model.LinearRegression()
+    #predictor = AdaBoostRegressor(base_estimator = linear_model.LinearRegression(), loss = 'square')
     predictor = AdaBoostRegressor(base_estimator = svm.SVR(kernel = 'poly', degree = x), loss = 'square')
     predictors.append( predictor)
     scores = cross_val_score(predictor, xsTrain, ys, cv = 10, scoring = our_scorer)
+    #scores = cross_val_score(predictor, xsTrainExtended, ys, cv = 10, scoring = our_scorer)
     print(numpy.mean(scores))
-'''
-predictor = AdaBoostRegressor(base_estimator = svm.SVR(kernel = 'poly', degree=4), loss = 'square')
 
-model = predictor.fit(xsTrain, ys)
-values = model.predict(dataTest)
-write_to_csv(values, "task1_out")
+#predictor = AdaBoostRegressor(base_estimator = svm.SVR(kernel = 'poly', degree=4), loss = 'square')
+
+
+
+#model = predictor.fit(xsTrain, ys)
+#values = model.predict(dataTest)
+
+#model = predictor.fit(xsTrainExtended, ys)
+#values = model.predict(dataTestExtended)
+
+#write_to_csv(values, "task1_out")
