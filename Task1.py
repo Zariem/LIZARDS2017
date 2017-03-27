533db017c224226e84876a7663d7727a7b86e306
DaJay42 <DaJay42@users.noreply.github.com>
2017-03-27 15:52:35 +0200

    use log of ys (cv score: -1.1)
    use log of ys (cv score: -1.1)

--- Task1.py
+++ Task1.py
@@ -37,12 +37,26 @@ def extendFeatures(data):
     for line in data:
         new_line = []
         for value in line:
-            new_line.append(value)
-            new_line.append(math.log(math.fabs(value)))
-            new_line.append(math.exp(value))
-            new_line.append(math.sqrt(math.fabs(value)))
-        new_line.append(math.fsum(new_line))
+            new_line.append(value) #-28
+            #new_line.append(math.log(math.fabs(value))) #-57
+            #new_line.append(math.log(math.fabs(value)) * value) #-47
+            #new_line.append(math.tanh(value)) #-60
+            #new_line.append(math.sin(value)) #-62
+            #new_line.append(1/value) #-65
+            #new_line.append(math.exp(value)) #-59
+            #new_line.append(math.exp(value**2.0)) #-59
+            #new_line.append(math.exp(-value)) #-59
+            #new_line.append(math.sqrt(math.fabs(value))) #-49
+            #new_line.append(math.asinh(value)) #-52
+            #new_line.append(math.cosh(value)) #-46
+            #new_line.append(math.sqrt(math.exp(value))) #-29 !!!
+            #new_line.append(math.sqrt(math.exp(-value)))
+        #new_line.append(math.fsum(line))
         bla.append(new_line)
+    return bla
+
+def transformOut(data):
+    bla = numpy.log(numpy.abs(data))
     return bla
 
 def apply_lin_reg(xTrain, yTrain, xTest):
@@ -62,12 +76,13 @@ def write_to_csv(values, filename):
 def error(real_value, prediction):
     return mean_squared_error(real_value, prediction)**0.5
 
-
+def exp_kernel(x, y):
+    return numpy.dot(numpy.exp(numpy.transpose(x),y))
 
 dataTrain = read_data()
 xsTrain = get_xsTrain(dataTrain)
 extendFeatures(xsTrain)
-ys = get_ys(dataTrain)
+ys = transformOut(get_ys(dataTrain))
 dataTest = read_data(filepathTest)
 
 xsTrainExtended = extendFeatures(xsTrain)
@@ -76,15 +91,16 @@ dataTestExtended = extendFeatures(dataTest)
 our_scorer = make_scorer(error, greater_is_better = False)
 predictors = []
 
-for x in range(2,7):
+for x in range(10,20):
     print(x)
-    #predictor = svm.SVR(kernel = 'poly', degree=x)
+    predictor = svm.SVR(kernel = 'poly', degree=2)
+    predictor.kernel
     #predictor = linear_model.LinearRegression()
     #predictor = AdaBoostRegressor(base_estimator = linear_model.LinearRegression(), loss = 'square')
-    predictor = AdaBoostRegressor(base_estimator = svm.SVR(kernel = 'poly', degree = x), loss = 'square')
+    #predictor = AdaBoostRegressor(base_estimator = svm.SVR(kernel = 'poly', degree = x), loss = 'square')
     predictors.append( predictor)
-    scores = cross_val_score(predictor, xsTrain, ys, cv = 10, scoring = our_scorer)
-    #scores = cross_val_score(predictor, xsTrainExtended, ys, cv = 10, scoring = our_scorer)
+    #scores = cross_val_score(predictor, xsTrain, ys, cv = 10, scoring = our_scorer)
+    scores = cross_val_score(predictor, xsTrainExtended, ys, cv = x, scoring = our_scorer)
     print(numpy.mean(scores))
 
 #predictor = AdaBoostRegressor(base_estimator = svm.SVR(kernel = 'poly', degree=4), loss = 'square')
